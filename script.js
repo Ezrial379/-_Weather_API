c049a440e09f82aebf13ad4d94ed61943525f6c3
Ezrial379 <112234930+Ezrial379@users.noreply.github.com>
2024-10-31 19:51:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,7 @@ async function fetchWeather() {
         const cnt = 10;
         const cityInputtedByUser = document.getElementById('cityInput').value;
 
-        const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInputtedByUser}&appid=${apiKey}&units=${units}&cnt=${cnt}`;
-
+        const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=${apiKey}`;
 
         const response = await fetch(apiUrl);
         const data = await response.json();
